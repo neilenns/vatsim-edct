d8b294ee15145ac42a1f89439b093dcad314d40f
Neil <neile@live.com>
2024-02-09 20:18:09 -0800

    Redirects if already logged in... kinda

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Box, Button, Typography } from "@mui/material";
+import { Navigate } from "react-router-dom";
 
 const WelcomePage = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   const handleLogin = async () => {
     await loginWithRedirect({
@@ -24,47 +25,53 @@ const WelcomePage = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-    >
-      <img
-        src="/appicon.svg"
-        alt="Airplane taking off"
-        width="128"
-        height="128"
-      />
-      <Typography variant="h3" gutterBottom>
-        Welcome
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        style={{ marginTop: "16px" }}
-        onClick={() => {
-          void (async () => {
-            await handleLogin();
-          })();
-        }}
-      >
-        Login
-      </Button>
-      <Button
-        size="large"
-        style={{ marginTop: "8px" }}
-        onClick={() => {
-          void (async () => {
-            await handleSignup();
-          })();
-        }}
-      >
-        Sign up
-      </Button>
-    </Box>
+    <div>
+      {isAuthenticated ? (
+        <Navigate to="/edct" replace={true} />
+      ) : (
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+        >
+          <img
+            src="/appicon.svg"
+            alt="Airplane taking off"
+            width="128"
+            height="128"
+          />
+          <Typography variant="h3" gutterBottom>
+            Welcome
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{ marginTop: "16px" }}
+            onClick={() => {
+              void (async () => {
+                await handleLogin();
+              })();
+            }}
+          >
+            Login
+          </Button>
+          <Button
+            size="large"
+            style={{ marginTop: "8px" }}
+            onClick={() => {
+              void (async () => {
+                await handleSignup();
+              })();
+            }}
+          >
+            Sign up
+          </Button>
+        </Box>
+      )}
+    </div>
   );
 };
 
